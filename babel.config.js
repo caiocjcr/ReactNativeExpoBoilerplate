bff6997534f3cccc302b4c052fc08a8a3ce73b44
Caio Rosa <caio.rosa@taniabulhoes.com.br>
2021-08-27 11:28:01 -0300

    chore: configure babel module resolver plugin

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,19 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
-  };
-};
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@': './src'
+          }
+        }
+      ]
+    ]
+  }
+}
